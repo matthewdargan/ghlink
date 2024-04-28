9ef191eeef39c2f20ffe445f6349b58b2f6ef16c
Matthew Dargan <matthewdargan57@gmail.com>
2024-04-28 10:56:03 -0500

    feat: parse args

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,8 @@ use clap::Parser;
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
 struct Args {
+    /// Sets a path to link
+    path: std::path::PathBuf,
     /// Print link to start line number
     #[arg(short, long)]
     start_line: Option<usize>,
@@ -14,8 +16,17 @@ struct Args {
     text: Option<String>,
 }
 
+const USAGE: &str = "usage: ghlink [-s start-line [-e end-line] | -t text] path";
+
 fn main() {
     let args = Args::parse();
+    match (args.start_line, args.end_line, &args.text) {
+        (None, None, None) => {}
+        (Some(_), None, None) => {}
+        (Some(_), Some(_), None) => {}
+        (None, None, Some(_)) => {}
+        _ => eprintln!("{}", USAGE),
+    }
     println!("start_line: {:?}", args.start_line);
     println!("end_line: {:?}", args.end_line);
     println!("text: {:?}", args.text);
