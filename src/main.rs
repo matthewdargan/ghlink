5eddc4a4b15aa97f1fcb92aedbeee430882c2b6a
Matthew Dargan <matthewdargan57@gmail.com>
2024-04-29 00:38:01 -0500

    refactor: replace clap with std::env

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,51 @@
-use clap::Parser;
 use std::error::Error;
 
-#[derive(Parser, Debug)]
-#[command(version, about, long_about = None)]
-struct Args {
-    /// Sets a path to link
-    path: std::path::PathBuf,
-    /// Print link to start line number
-    #[arg(short, long)]
-    start_line: Option<usize>,
-    /// Print link to end line number
-    #[arg(short, long)]
-    end_line: Option<usize>,
-    /// Print link to matching text
-    #[arg(short, long)]
-    text: Option<String>,
+#[derive(Debug)]
+struct Cli {
+    l1: Option<usize>,
+    l2: Option<usize>,
+    search: Option<String>,
+    path: Option<std::path::PathBuf>,
 }
 
-const USAGE: &str = "usage: ghlink [-s start-line [-e end-line] | -t text] path";
+const USAGE: &str = "usage: ghlink [-l1 line1 [-l2 line2] | -s text] path";
+
+fn parse_args() -> Option<Cli> {
+    let mut args = std::env::args().skip(1);
+    let mut cli = Cli {
+        l1: None,
+        l2: None,
+        search: None,
+        path: None,
+    };
+    while let Some(arg) = args.next() {
+        match arg.as_str() {
+            "-l1" => {
+                cli.l1 = Some(args.next()?.parse::<usize>().ok()?);
+            }
+            "-l2" => {
+                cli.l2 = Some(args.next()?.parse::<usize>().ok()?);
+            }
+            "-s" => cli.search = args.next(),
+            _ => cli.path = Some(std::path::PathBuf::from(arg)),
+        }
+    }
+    if cli.l1.is_none() && cli.l2.is_some() {
+        return None;
+    }
+    cli.path.as_ref()?;
+    Some(cli)
+}
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let args = Args::parse();
-    match (args.start_line, args.end_line, &args.text) {
-        (None, None, None) => {}
-        (Some(_), None, None) => {}
-        (Some(_), Some(_), None) => {}
-        (None, None, Some(_)) => {}
-        _ => eprintln!("{}", USAGE),
-    }
-    let mut path = std::fs::canonicalize(&args.path)?;
+    let cli = match parse_args() {
+        Some(cli) => cli,
+        None => {
+            eprintln!("{}", USAGE);
+            return Ok(());
+        }
+    };
+    let mut path = std::fs::canonicalize(cli.path.as_ref().unwrap())?;
     if path.is_file() {
         path.pop();
     }
@@ -38,6 +55,7 @@ fn main() -> Result<(), Box<dyn Error>> {
         .unwrap()?;
     let url = &remote.url(gix::remote::Direction::Fetch).unwrap().path;
     let commit = repo.rev_parse_single("HEAD")?;
+    println!("{:?}", cli);
     println!("{:?}", path);
     println!("{:?}", url);
     println!("{:?}", commit);
