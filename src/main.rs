b2696c710bd30544f39fe8154b1529786c00f2ed
Matthew Dargan <matthewdargan57@gmail.com>
2024-04-30 12:57:00 -0500

    refactor: std imports

--- src/main.rs
+++ src/main.rs
@@ -2,21 +2,25 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
+use std::env;
 use std::error::Error;
-use std::io::BufRead;
+use std::fs::{self, File};
+use std::io::{self, BufRead, BufReader};
+use std::path::{Path, PathBuf};
+use std::str;
 
 #[derive(Debug)]
 struct Cli {
     l1: Option<usize>,
     l2: Option<usize>,
     search: Option<String>,
-    path: Option<std::path::PathBuf>,
+    path: Option<PathBuf>,
 }
 
 const USAGE: &str = "usage: ghlink [-l1 line1 [-l2 line2] | -s text] file";
 
 fn parse_args() -> Option<Cli> {
-    let mut args = std::env::args().skip(1);
+    let mut args = env::args().skip(1);
     let mut cli = Cli {
         l1: None,
         l2: None,
@@ -32,7 +36,7 @@ fn parse_args() -> Option<Cli> {
                 cli.l2 = Some(args.next()?.parse::<usize>().ok()?);
             }
             "-s" => cli.search = args.next(),
-            _ => cli.path = Some(std::path::PathBuf::from(arg)),
+            _ => cli.path = Some(PathBuf::from(arg)),
         }
     }
     if cli.l1.is_none() && cli.l2.is_some() {
@@ -45,8 +49,8 @@ fn parse_args() -> Option<Cli> {
     Some(cli)
 }
 
-fn search_lines(path: &std::path::Path, text: &str) -> std::io::Result<Vec<usize>> {
-    let reader = std::io::BufReader::new(std::fs::File::open(path)?);
+fn search_lines(path: &Path, text: &str) -> io::Result<Vec<usize>> {
+    let reader = BufReader::new(File::open(path)?);
     let mut text_lines = text.lines().peekable();
     let mut line_nums = Vec::new();
     for (i, line) in reader.lines().enumerate() {
@@ -58,8 +62,8 @@ fn search_lines(path: &std::path::Path, text: &str) -> std::io::Result<Vec<usize
         }
     }
     if line_nums.is_empty() {
-        return Err(std::io::Error::new(
-            std::io::ErrorKind::NotFound,
+        return Err(io::Error::new(
+            io::ErrorKind::NotFound,
             format!("file {} does not contain string {}", path.display(), text),
         ));
     }
@@ -75,7 +79,7 @@ fn main() -> Result<(), Box<dyn Error>> {
         }
     };
     let path = cli.path.unwrap();
-    let mut abs_path = std::fs::canonicalize(&path)?;
+    let mut abs_path = fs::canonicalize(&path)?;
     if abs_path.is_file() {
         abs_path.pop();
     }
@@ -89,7 +93,7 @@ fn main() -> Result<(), Box<dyn Error>> {
         .path
         .strip_suffix(".git".as_bytes())
         .unwrap();
-    let git_path_str = std::str::from_utf8(git_path)?;
+    let git_path_str = str::from_utf8(git_path)?;
     let commit = repo.rev_parse_single("HEAD")?;
     let prefix = repo.prefix()?;
     let joined = prefix.unwrap().join(&path);
@@ -106,7 +110,7 @@ fn main() -> Result<(), Box<dyn Error>> {
     }
     if let Some(mut search) = cli.search {
         if search == "-" {
-            search = std::io::read_to_string(std::io::stdin())?;
+            search = io::read_to_string(io::stdin())?;
         }
         let line_nums = search_lines(path.as_path(), &search)?;
         url.push_str(&format!("#L{}", line_nums.first().unwrap()));
