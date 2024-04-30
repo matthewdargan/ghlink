7b923e40338b0032ca9aec5a94dc887d932e7d90
Matthew Dargan <matthewdargan57@gmail.com>
2024-04-29 23:56:33 -0500

    chore: add license header

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+// Copyright 2024 Matthew P. Dargan. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 use std::error::Error;
 use std::io::BufRead;
 
