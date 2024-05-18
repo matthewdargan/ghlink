3084c34ced744d906d6700de3bd4806c63553c00
Matthew Dargan <matthewdargan57@gmail.com>
2024-05-18 10:46:13 -0500

    docs: update gix_remote_url

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,26 @@
+// Copyright 2024 Matthew P. Dargan. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 use std::str::{self, Utf8Error};
 
+/// Returns the URL of the remote repository.
+///
+/// # Errors
+///
+/// This function will return an error if the URL is not valid UTF-8.
+///
+/// # Examples
+///
+/// ```
+/// use ghlink::gix_remote_url;
+///
+/// let repo = gix::open(".")?;
+/// let remote = repo
+///     .find_default_remote(gix::remote::Direction::Fetch)
+///     .unwrap()?;
+/// let git_path_str = gix_remote_url(&remote)?.unwrap();
+/// ```
 pub fn gix_remote_url(remote: &gix::Remote) -> Result<Option<String>, Utf8Error> {
     let git_path = match remote.url(gix::remote::Direction::Fetch) {
         Some(url) => match url.path.strip_suffix(b".git") {
